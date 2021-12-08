ae916aa8751095875f420224f4de12e880dd7c89
garcialopezj445 <garcialopezj445@gmail.com>
2021-12-08 16:59:13 -0500

    Se agrega la función de actualizar perfil de usuario

--- src/routes/authenticate.js
+++ src/routes/authenticate.js
@@ -42,10 +42,7 @@ router.post('/login', async (req, res) => {
     const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ? and password = ?', [identificacion_usuario, clave]);
     if(prueba.length > 0){
         req.flash('realizado', 'Sesión validada');
-        //const user = prueba[0];
         res.render('profile', {user: prueba[0]});
-        //res.json(prueba[0]);
-        //res.render('linksUser'); pendiente
     }else{
         req.flash('incorrecto', 'Valores incorrectos');
         res.redirect('/login');
@@ -53,21 +50,45 @@ router.post('/login', async (req, res) => {
 
 });
 
-/*
-router.get('/profile', async (req, res) =>{
-    const links = await pool.query('SELECT * FROM servicio');
-    console.log(links);
-    res.render('links/service', { links });
-});
-
-*/
-
 router.get('/registros/:identificacion_usuario', async(req, res) => {
     const {identificacion_usuario} = req.params;
     console.log(identificacion_usuario);
     const registrosUsuario = await pool.query('SELECT * FROM servicio WHERE identificacion_servicio_usuario = ?', [identificacion_usuario]);
     console.log(registrosUsuario);
     res.render('links/service', { registrosUsuario });
+});
+
+router.get('/editUser/:identificacion_usuario', async(req, res) => {
+    const {identificacion_usuario} = req.params;
+    const usuario = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
+    console.log(usuario[0]);
+    res.render('editUser', { usuario: usuario[0] });
+});
+
+router.post('/editUser/:identificacion_usuario', async (req, res) => {
+    const {identificacion_usuario} = req.params;
+    const { 
+        nombre_usuario,
+        password, 
+        telefono_usuario,
+        codigo_residencia_usuario,
+        fecha_nacimiento_usuario,
+        codigo_sexo_usuario
+    } = req.body;
+    const usuario = {
+        nombre_usuario,
+        password, 
+        telefono_usuario,
+        codigo_residencia_usuario,
+        fecha_nacimiento_usuario,
+        codigo_sexo_usuario
+    };
+    console.log(usuario);
+    await pool.query('UPDATE usuario set ? WHERE identificacion_usuario = ?', [usuario, identificacion_usuario]);
+    const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
+    console.log(prueba);
+    req.flash('realizado', 'Registro actualizado correctamente');
+    res.render('profile', {user: prueba[0]});
 });
 
 module.exports = router;

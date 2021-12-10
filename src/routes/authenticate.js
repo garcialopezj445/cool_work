89012d4d2bd0dff91a09043e48a975dbcdefa05f
garcialopezj445 <garcialopezj445@gmail.com>
2021-12-09 22:46:15 -0500

    Se agregan funcionalidades de navegación

--- src/routes/authenticate.js
+++ src/routes/authenticate.js
@@ -52,16 +52,13 @@ router.post('/login', async (req, res) => {
 
 router.get('/registros/:identificacion_usuario', async(req, res) => {
     const {identificacion_usuario} = req.params;
-    console.log(identificacion_usuario);
     const registrosUsuario = await pool.query('SELECT * FROM servicio WHERE identificacion_servicio_usuario = ?', [identificacion_usuario]);
-    console.log(registrosUsuario);
     res.render('links/service', { registrosUsuario });
 });
 
 router.get('/editUser/:identificacion_usuario', async(req, res) => {
     const {identificacion_usuario} = req.params;
     const usuario = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
-    console.log(usuario[0]);
     res.render('editUser', { usuario: usuario[0] });
 });
 
@@ -83,11 +80,15 @@ router.post('/editUser/:identificacion_usuario', async (req, res) => {
         fecha_nacimiento_usuario,
         codigo_sexo_usuario
     };
-    console.log(usuario);
     await pool.query('UPDATE usuario set ? WHERE identificacion_usuario = ?', [usuario, identificacion_usuario]);
     const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
-    console.log(prueba);
     req.flash('realizado', 'Registro actualizado correctamente');
+    res.render('profile', {user: prueba[0]});
+});
+
+router.get('/volver/:identificacion_usuario', async (req, res) => {
+    const {identificacion_usuario} = req.params;
+    const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
     res.render('profile', {user: prueba[0]});
 });
 
--- src/routes/links.js
+++ src/routes/links.js
@@ -8,86 +8,85 @@ router.get('/', (req, res) => {
     res.render('links/homeUser');
 });
 
-router.get('/add', (req, res) =>{
-    res.render('links/add');
+router.get('/add/:identificacion_usuario', async (req, res) => {
+    const {identificacion_usuario} = req.params;
+    console.log(identificacion_usuario);
+    const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_usuario]);
+    res.render('links/add', { user: prueba[0] });
 });
 
-router.post('/add', async (req, res) =>{
+router.post('/add/:identificacion_usuario', async (req, res) => {
     const {
-        nombre_servicio, 
+        nombre_servicio,
         identificacion_servicio_usuario,
-        descripcion_servicio, 
+        descripcion_servicio,
         codigo_servicio_categoria
     } = req.body;
     const newRegistro = {
-        nombre_servicio, 
+        nombre_servicio,
         identificacion_servicio_usuario,
-        descripcion_servicio, 
+        descripcion_servicio,
         codigo_servicio_categoria
     };
     console.log(newRegistro);
     await pool.query('INSERT INTO servicio set ?', [newRegistro]);
     const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_servicio_usuario]);
     req.flash('realizado', 'Registro guardado correctamente');
-    res.render('profile', {user: prueba[0]});
+    res.render('profile', { user: prueba[0] });
 });
 
 
-router.get('/added-links', async (req, res) =>{
-   const links = await pool.query('SELECT * FROM servicio');
+router.get('/added-links', async (req, res) => {
+    const links = await pool.query('SELECT * FROM servicio');
     console.log(links);
     res.render('links/service', { links });
 });
 
-router.get('/edit/:identificacion_registro', async (req, res) =>{
-    const {identificacion_registro} = req.params;
+router.get('/edit/:identificacion_registro', async (req, res) => {
+    const { identificacion_registro } = req.params;
     const registrosUsuario = await pool.query('SELECT * FROM servicio WHERE identificacion_registro = ?', [identificacion_registro]);
-    console.log(registrosUsuario[0]);
     res.render('links/edit', { registrosUsuario: registrosUsuario[0] });
 });
 
 router.post('/edit/:identificacion_registro', async (req, res) => {
-    const {identificacion_registro} = req.params;
+    const { identificacion_registro } = req.params;
     const {
-        nombre_servicio, 
+        nombre_servicio,
         identificacion_servicio_usuario,
-        descripcion_servicio, 
+        descripcion_servicio,
         codigo_servicio_categoria
     } = req.body;
     const newRegistro = {
-        nombre_servicio, 
+        nombre_servicio,
         identificacion_servicio_usuario,
-        descripcion_servicio, 
+        descripcion_servicio,
         codigo_servicio_categoria
     }
-    console.log(newRegistro);
     await pool.query('UPDATE servicio set ? WHERE identificacion_registro = ?', [newRegistro, identificacion_registro]);
     const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_servicio_usuario]);
-    console.log(prueba);
     req.flash('realizado', 'Registro actualizado correctamente');
-    res.render('profile', {user: prueba[0]});
+    res.render('profile', { user: prueba[0] });
 });
 
 router.get('/delete/:identificacion_registro', async (req, res) => {
-    const {identificacion_registro} = req.params;
+    const { identificacion_registro } = req.params;
     const registrosUsuario = await pool.query('SELECT * FROM servicio WHERE identificacion_registro = ?', [identificacion_registro]);
     console.log(registrosUsuario[0]);
-    res.render('links/delete', { registrosUsuario: registrosUsuario[0] });  
+    res.render('links/delete', { registrosUsuario: registrosUsuario[0] });
 });
 
 router.post('/delete/:identificacion_registro', async (req, res) => {
-    const {identificacion_registro} = req.params;
+    const { identificacion_registro } = req.params;
     const {
         identificacion_servicio_usuario,
     } = req.body;
-    const registro = { 
+    const registro = {
         identificacion_servicio_usuario,
     }
     const prueba = await pool.query('SELECT * FROM usuario WHERE identificacion_usuario = ?', [identificacion_servicio_usuario]);
-    console.log(prueba);
     await pool.query('DELETE FROM servicio WHERE identificacion_registro = ?', [identificacion_registro]);
     req.flash('realizado', 'Registro eliminado correctamente');
-    res.render('profile', {user: prueba[0]});
+    res.render('profile', { user: prueba[0] });
 });
 
 
